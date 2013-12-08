ecded9278d34df75fd50c5eed637f2943a88135f
Wouter van den Heuvel <exo_00@hotmail.com>
2013-12-08 18:49:02 +0100

    Leaderboard. Press 'l' to bring up. See GuiMenu.js for an example for realtime update. Also introduced some medal display, the medal images will have to be changed ofc

---
+++ client/js/ui/Leaderboard.js
@@ -0,0 +1,67 @@
+var Leaderboard = function(parent, data){
+	// Call parent constructor
+	Popup.prototype.constructor.call(this, parent);
+
+	this.el.append( '' +
+		'<div class="leaderboard"> ' +
+		'<h1>Leaderboard</h1>' +
+		'<table cellpadding="0" cellspacing="0" border="0" id="leaderboard"></table>' +
+		'</div>' );
+	// Used in column renderer
+	var parseIcons = function(icons){
+		var res = "<div class='medals'>";
+		var css = {
+			bronze1: "medal_bronze1",
+			bronze2: "medal_bronze2",
+			bronze3: "medal_bronze3",
+			silver1: "medal_silver1",
+			silver2: "medal_silver2",
+			silver3: "medal_silver3",
+			gold1: "medal_gold1",
+			gold2: "medal_gold2",
+			gold3:  "medal_gold3"
+		}
+		for(var i in icons){
+			if(icons[i]){
+				res += "<div title='" + i + "' class='" + css[i] + "'></div>";
+			}
+		}
+		return res + "</div>";
+	}
+
+	$('#leaderboard').dataTable( {
+		"aaData": data,
+		"aoColumns": [{
+			sTitle: "Name", mRender: function ( data, type, full ) {
+				return data + '' + parseIcons(full[5]);
+			}
+		},
+			{ "sTitle": "Citizens", sClass: "right_align", sWidth: "100px" },
+			{ "sTitle": "Culture", sClass: "right_align", sWidth: "100px" },
+			{ "sTitle": "Economy", sClass: "right_align", sWidth: "100px" },
+			{ "sTitle": "Knowledge", sClass: "right_align", sWidth: "110px" }
+		],
+		bLengthChange: false,
+		bInfo: false,
+		bPaginate: false,
+		bFilter: false
+	});
+	$('#leaderboard th').append('<div></div>');
+	$(window).bind('resize', function () {
+		$('#leaderboard').css('width', '100%');
+	} );
+
+	this.el.hide();
+}
+Leaderboard.prototype = new Popup();
+
+/** Updates data, provide data in nested array */
+Leaderboard.prototype.update = function(data){
+	// Prevent updating if not needed to improve performance. Also Chrome doesnt like it
+	if(!this.isVisible()) return false;
+
+	for(var i = 0; i < data.length; i++){
+		$('#leaderboard').dataTable().fnUpdate(data[i], i);
+	}
+}
+Leaderboard.prototype.constructor = Leaderboard;
--- client/js/ui/Popup.js
+++ client/js/ui/Popup.js
@@ -4,80 +4,11 @@ var Popup = function(parent) {
 	this.el.draggable({ containment: "window" });
 	this.el.resizable();
 
-	var getRndInt = function(max){
-		return Math.floor(Math.random() * max);
-	}
 
-	var getRndIcons = function() {
-		return {
-			bronze1: Math.random() < .5,
-			bronze2: Math.random() < .5,
-			bronze3: Math.random() < .5,
-			silver1: Math.random() < .5,
-			silver2: Math.random() < .5,
-			silver3: Math.random() < .5,
-			gold1: Math.random() < .5,
-			gold2: Math.random() < .5,
-			gold3: Math.random() < .5
-		};
-	}
-
-	var parseIcons = function(icons){
-		var res = "<div class='medals'>";
-		var css = {
-			bronze1: "medal_bronze1",
-			bronze2: "medal_bronze2",
-			bronze3: "medal_bronze3",
-			silver1: "medal_silver1",
-			silver2: "medal_silver2",
-			silver3: "medal_silver3",
-			gold1: "medal_gold1",
-			gold2: "medal_gold2",
-			gold3:  "medal_gold3"
-		}
-		for(var i in icons){
-			if(icons[i]){
-				res += "<div class='" + css[i] + "'></div>";
-			}
-		}
-		return res + "</div>";
-	}
-
-	this.el.html( '<table cellpadding="0" cellspacing="0" border="0" class="leaderboard" id="example"></table>' );
-	$('#example').dataTable( {
-		"aaData": [
-			/* Reduced data set */
-			[ "Han Solo", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Luke Skywalker", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Leia Organa", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Boba Fett", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Darth Vader", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Yoda", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Chewbacca", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Obi Wan Kenobi", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Jabba the Hut", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ],
-			[ "Lando Calrissian", getRndInt(40), getRndInt(2000), getRndInt(2000), getRndInt(2000) ]
-		],
-		"aoColumns": [
-			{ sTitle: "Name", mRender: function ( data, type, full ) {
-					return data + '' + parseIcons(getRndIcons());
-				}
-			},
-			{ "sTitle": "Citizens", sClass: "rightAlign", sWidth: "80px" },
-			{ "sTitle": "Culture", sClass: "rightAlign", sWidth: "80px" },
-			{ "sTitle": "Economy", sClass: "rightAlign", sWidth: "80px" },
-			{ "sTitle": "Knowledge", sClass: "rightAlign", sWidth: "80px" }
-		],
-		bLengthChange: false,
-		bInfo: false,
-		bPaginate: false,
-		bFilter: false
-	} );
 
 	var closeButton = $("<div class='close_button'></div>");
 	closeButton.click($.proxy(this.hide, this));
 	this.el.append(closeButton);
-	this.show();
 }
 
 Popup.prototype = {
@@ -99,7 +30,17 @@ Popup.prototype = {
 	},
 
 	hide: function(){
-		this.el.hide();
+		this.el.hide("slow");
+	},
+
+	/** Toggles visibility */
+	toggle: function(){
+		this.el.toggle( "slow" );
+	},
+
+	/** Returns true if this popup is visible */
+	isVisible: function(){
+		return this.el.is(':visible');
 	}
 }
 
