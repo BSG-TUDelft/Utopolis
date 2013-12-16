8d34c017200825e88dd8fc282c9f7668ca3251a5
spassvogel <exo_00@hotmail.com>
2013-12-17 00:54:20 +0100

    Context menu now shows revenue. depending on how many citizens have been assigned

--- client/bsg/ui/ContextMenu.js
+++ client/bsg/ui/ContextMenu.js
@@ -1,17 +1,14 @@
 "use strict";
 /**
  * @param parent parent element to add popup to
- * @param options Options object, valid
- * 		options are:
- * 	* noDrag (Boolean)
- * 	* noClose (Boolean)
+ * @param data
  * @constructor
  */
 var ContextMenu = function(parent, data) {
 	this.el = $("<div class='contextmenu'></div>");
 	$(parent).append(this.el);
 	this.data = data;
-}
+};
 
 ContextMenu.prototype = {
 
@@ -22,60 +19,83 @@ ContextMenu.prototype = {
 	animation: 0,
 
 	/** Shows this ContextMenu with structure info
-	 * @param structure	(Structure) */
-	show: function(structure) {
-		function citizenFormatter(){
+	 * @param structure    (Structure) */
+	show: function (structure) {
+		function citizenFormatter() {
 			return $.format('Citizens allocated: {0}', structure.citizens);
 		}
-		var structureInfo = this.getStructureInfo(structure.name);
-		if(structureInfo === null)
-			console.log("No structure info found with structureId " + structure.name);
-		var structureTypeInfo = this.data.structureTypes[structureInfo.structureType];
 
-		var html = "<h2>" + structureInfo.name + "</h2>" +
-			"<div class='structureicon " + structureInfo.iconCss + "'></div>" +
-			"<span id='citizencounter'>" + citizenFormatter() + "</span>" +
-			"<div id='structureslider'></div>";
+		var structureInfo = this.getStructureInfo(structure.name);
+		if (structureInfo === null)
+			console.log("No structure info found with structureId " + structure.name);
+		var structureType = this.data.structureTypes[structureInfo.structureType];
+
+		var html = ["<h2>" + structureInfo.name + "</h2>",
+			"<div class='structureicon " + structureInfo.iconCss + "'></div>",
+			"<span id='citizencounter'>" + citizenFormatter() + "</span>",
+			"<div id='structureslider'></div>",
+			"<h3>Generates (every X time): </h3>",
+			"<ul class='generates'>",
+			"</div>",
+			"</ul>"];
 
 		this.el.html(html);
+		this.updateRevenue(structureType, structure);
 
-		$( "#structureslider" ).slider({
+		$("#structureslider").slider({
 			value: structure.citizens,
 			min: 0,
-			max: structureTypeInfo.citizenCap,
+			max: structureType.citizenCap,
 			step: 1,
-			slide: function( event, ui ) {
+			slide: $.proxy(function (event, ui) {
 				structure.citizens = ui.value;
-				$( "#citizencounter" ).html(citizenFormatter());
-			}
+				this.updateRevenue(structureType, structure);
+				$("#citizencounter").html(citizenFormatter());
+			}, this)
 		});
+
 		this.el.show();
 	},
 
 	/** Hides this ContextMenu */
-	hide: function(){
+	hide: function () {
 		this.el.hide();
 	},
 
 	/** Toggles visibility */
-	toggle: function(){
+	toggle: function () {
 		this.el.toggle();
 	},
 
 	/** Returns true if this ContextMenu is visible */
-	isVisible: function(){
+	isVisible: function () {
 		return this.el.is(':visible');
 	},
 
 	/** Returns structure type info by structureId
- 	 * @param structureId (String) for example: hele_house */
- 	getStructureInfo: function(structureId){
-		for(var e = 0; e < this.data.empires.length; e++){
-			for(var s = 0; s < this.data.empires[e].structures.length; s++){
-				if(this.data.empires[e].structures[s].structureId == structureId)
+	 * @param structureId (String) for example: hele_house */
+	getStructureInfo: function (structureId) {
+		for (var e = 0; e < this.data.empires.length; e++) {
+			for (var s = 0; s < this.data.empires[e].structures.length; s++) {
+				if (this.data.empires[e].structures[s].structureId == structureId)
 					return this.data.empires[e].structures[s];
 			}
 		}
+		return null;
+	},
+
+	updateRevenue: function(structureType, structure) {
+		// Add revenue
+		var html = [];
+		for (var i in structureType.generates) {
+			if (!structureType.generates.hasOwnProperty(i)) continue;
+
+			html.push("<li class='" + this.data.resources[i].iconCss + "' >",
+				this.data.resources[i].name + ": <span>",
+				structureType.generates[i] * structure.citizens,
+				"</span></li>");
+		}
+		this.el.find(".generates").html(html.join(''));
 	}
-}
+};
 
