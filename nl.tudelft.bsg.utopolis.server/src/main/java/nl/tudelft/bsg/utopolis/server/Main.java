aac9210dcdc714ad397998bb867f99dcdc9dd9fd
spassvogel <exo_00@hotmail.com>
2014-02-20 11:24:35 +0100

    Game now reads configuration file for structure info

--- nl.tudelft.bsg.utopolis.server/src/main/java/nl/tudelft/bsg/utopolis/server/Main.java
+++ nl.tudelft.bsg.utopolis.server/src/main/java/nl/tudelft/bsg/utopolis/server/Main.java
@@ -11,6 +11,8 @@ import javax.ws.rs.ext.ContextResolver;
 
 import nl.tudelft.bsg.utopolis.server.db.DBBootstrap;
 import nl.tudelft.bsg.utopolis.server.db.DBConnector;
+import nl.tudelft.bsg.utopolis.server.model.Resources;
+import nl.tudelft.bsg.utopolis.server.model.util.StructureProperties;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -35,14 +37,77 @@ public class Main {
      */
     public static HttpServer startServer() {
 		/*
-		 * Todo: read structure properties here, store in application variable*/
+		 * Todo: read structure properties here, store in application variable
 		
 		FileReader reader;
 		try {
 			reader = new FileReader( "src/main/config/structureproperties.json");
-			JsonObject jsonObject = JsonObject.readFrom(reader);
-			String k = jsonObject.get("house").toString();
-			System.out.println(k);
+			JsonObject allProperties = JsonObject.readFrom(reader);
+			JsonObject structProps = allProperties.get("house").asObject();
+			Resources cost = new Resources(),
+					requirements = new Resources(),
+					generates = new Resources();
+
+			
+			// Parse cost
+			JsonObject costProps = structProps.get("cost").asObject();
+			if(costProps != null){			
+				float wood = parseNullableFloat(costProps, "wood");
+				float food = parseNullableFloat(costProps, "food");
+				float metal = parseNullableFloat(costProps, "metal");
+				float stone = parseNullableFloat(costProps, "stone"); 
+
+				cost.setWood(wood);
+				cost.setFood(food);
+				cost.setMetal(metal);
+				cost.setStone(stone);
+			}
+
+			// Parse requirements
+			JsonObject reqProps = structProps.get("requirements").asObject();
+			if(reqProps != null){			
+				float knowledge = parseNullableFloat(costProps, "knowledge");
+				float culture = parseNullableFloat(costProps, "culture");
+
+				requirements.setWood(knowledge);
+				requirements.setFood(culture);
+			}
+			
+			// Parse generates
+			JsonObject genProps = structProps.get("generates").asObject();
+			if(genProps != null){			
+				float wood = parseNullableFloat(costProps, "wood");
+				float food = parseNullableFloat(costProps, "food");
+				float metal = parseNullableFloat(costProps, "metal");
+				float stone = parseNullableFloat(costProps, "stone"); 
+				float knowledge = parseNullableFloat(costProps, "knowledge");
+				float culture = parseNullableFloat(costProps, "culture");
+				float spirituality = parseNullableFloat(costProps, "spirituality");
+				float safety = parseNullableFloat(costProps, "safety");
+				float peace = parseNullableFloat(costProps, "peace");
+				float diplomacy = parseNullableFloat(costProps, "diplomacy");
+				float trade = parseNullableFloat(costProps, "trade");
+				float economy = parseNullableFloat(costProps, "economy");
+
+				generates.setWood(wood);
+				generates.setFood(food);
+				generates.setMetal(metal);
+				generates.setStone(stone);
+				generates.setKnowledge(knowledge);
+				generates.setCulture(culture);
+				generates.setSpirituality(spirituality);
+				generates.setSafety(safety);
+				generates.setPeace(peace);
+				generates.setDiplomacy(diplomacy);
+				generates.setTrade(trade);
+				generates.setEconomy(economy);
+			}
+			
+			long buildTime = structProps.get("buildTime").asLong();
+			int citizenCap = structProps.get("citizenCap").asInt();
+			
+			StructureProperties properties = new StructureProperties(cost, requirements, generates, buildTime, citizenCap);
+		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -62,7 +127,7 @@ public class Main {
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
     
-    public static ContextResolver<MoxyJsonConfig> createMoxyJsonResolver() {
+	public static ContextResolver<MoxyJsonConfig> createMoxyJsonResolver() {
         final MoxyJsonConfig moxyJsonConfig = new MoxyJsonConfig();
         Map<String, String> namespacePrefixMapper = new HashMap<String, String>(1);
         namespacePrefixMapper.put("http://www.w3.org/2001/XMLSchema-instance", "xsi");
@@ -84,6 +149,15 @@ public class Main {
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
         server.stop();
+    }
+
+    /** Tries to parse a float value corresponding to propertyName in given propertyObject
+     * @param propertyObject JsonObject 
+     * @param propertyName
+     * @return Float value if property is found, 0 if no property can be found */
+    private static float parseNullableFloat(JsonObject propertyObject, String propertyName) {
+    	
+    	return propertyObject.get(propertyName) == null ? 0f : propertyObject.get(propertyName).asFloat();
     }
 }
 
--- nl.tudelft.bsg.utopolis.server/src/main/java/nl/tudelft/bsg/utopolis/server/model/util/StructureProperties.java
+++ nl.tudelft.bsg.utopolis.server/src/main/java/nl/tudelft/bsg/utopolis/server/model/util/StructureProperties.java
@@ -3,11 +3,7 @@ package nl.tudelft.bsg.utopolis.server.model.util;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-
 import com.eclipsesource.json.JsonObject;
-import com.eclipsesource.json.JsonValue;
-
 import nl.tudelft.bsg.utopolis.server.model.Resources;
 import nl.tudelft.bsg.utopolis.server.model.Structure;
 
@@ -29,342 +25,83 @@ public class StructureProperties {
 	
 	public static StructureProperties getProperties(Structure s) {
 		FileReader reader;
+		
 		String sid = s.getStructureId();
 		String structureTypeId = sid.substring(sid.lastIndexOf('_')+1);
-
 		try {
 			reader = new FileReader( "src/main/config/structureproperties.json");
 			JsonObject allProperties = JsonObject.readFrom(reader);
-			JsonObject structProps = allProperties.get("house").asObject();
-			JsonObject requirements = structProps.get("requirements").asObject();
-			JsonObject cost = structProps.get("cost").asObject();
-			JsonObject generates = structProps.get("generates").asObject();
+			JsonObject structProps = allProperties.get(structureTypeId).asObject();
+			Resources cost = new Resources(),
+					requirements = new Resources(),
+					generates = new Resources();
+
 			
-			return new StructureProperties(
-					new Resources(2, 0, 0, 1, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 4, 4, 0, 0), 
-					new Resources(1, 1, 1, 0, 1, 1, 1, 1), 
-					30000, 
-					4);
+			// Parse cost
+			JsonObject costProps = structProps.get("cost").asObject();
+			if(costProps != null){			
+				float wood = parseNullableFloat(costProps, "wood");
+				float food = parseNullableFloat(costProps, "food");
+				float metal = parseNullableFloat(costProps, "metal");
+				float stone = parseNullableFloat(costProps, "stone"); 
+
+				cost.setWood(wood);
+				cost.setFood(food);
+				cost.setMetal(metal);
+				cost.setStone(stone);
+			}
+
+			// Parse requirements
+			JsonObject reqProps = structProps.get("requirements").asObject();
+			if(reqProps != null){			
+				float knowledge = parseNullableFloat(costProps, "knowledge");
+				float culture = parseNullableFloat(costProps, "culture");
+
+				requirements.setWood(knowledge);
+				requirements.setFood(culture);
+			}
 			
-			Resources resources = new Resources();
-			resources.setCitizens(0);
-			resources.setCulture(.);
-			System.out.println(k);
+			// Parse generates
+			JsonObject genProps = structProps.get("generates").asObject();
+			if(genProps != null){			
+				float wood = parseNullableFloat(costProps, "wood");
+				float food = parseNullableFloat(costProps, "food");
+				float metal = parseNullableFloat(costProps, "metal");
+				float stone = parseNullableFloat(costProps, "stone"); 
+				float knowledge = parseNullableFloat(costProps, "knowledge");
+				float culture = parseNullableFloat(costProps, "culture");
+				float spirituality = parseNullableFloat(costProps, "spirituality");
+				float safety = parseNullableFloat(costProps, "safety");
+				float peace = parseNullableFloat(costProps, "peace");
+				float diplomacy = parseNullableFloat(costProps, "diplomacy");
+				float trade = parseNullableFloat(costProps, "trade");
+				float economy = parseNullableFloat(costProps, "economy");
+
+				generates.setWood(wood);
+				generates.setFood(food);
+				generates.setMetal(metal);
+				generates.setStone(stone);
+				generates.setKnowledge(knowledge);
+				generates.setCulture(culture);
+				generates.setSpirituality(spirituality);
+				generates.setSafety(safety);
+				generates.setPeace(peace);
+				generates.setDiplomacy(diplomacy);
+				generates.setTrade(trade);
+				generates.setEconomy(economy);
+			}
+			
+			long buildTime = structProps.get("buildTime").asLong();
+			int citizenCap = structProps.get("citizenCap").asInt();
+			
+			return new StructureProperties(cost, requirements, generates, buildTime, citizenCap);
+		
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-
-		if (sid.endsWith("house")) {
-			return new StructureProperties(
-						new Resources(2, 0, 0, 1, 0, 0, 0, 0), 
-						new Resources(0, 0, 0, 0, 4, 4, 0, 0), 
-						new Resources(1, 1, 1, 0, 1, 1, 1, 1), 
-						30000, 
-						4);
-			/*
-			food, metal, stone, wood,    knowledge, culture, citizens, societal
-			*/
-		} else if (sid.endsWith("farm")) {
-			return new StructureProperties(
-					new Resources(2, 2, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 4, 4, 0, 0), 
-					new Resources(2, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			cost: {
-				metal: 2,
-				food: 2
-			},
-			requirements: {
-				knowledge: 4,
-				culture: 4
-			},
-			buildTime: 30000,
-			generates: {
-				food: 2,
-				health: 1,
-				economy: 1,
-				trade: 1
-			},
-			citizenCap: 6
-			 */
-		} else if (sid.endsWith("corral")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			cost: {
-				metal: 4,
-				food: 2
-			},
-			requirements: {
-				knowledge: 4,
-				culture: 4
-			},
-			buildTime: 30000,
-			generates: {
-				food: 4,
-				health: 1,
-				diplomacy: 1
-			},
-			citizenCap: 6
-			 */
-		} else if (sid.endsWith("market")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				metal: 100,
-				stone: 100,
-				wood: 1000,
-				food: 5000
-			},
-			requirements: {
-				knowledge: 100,
-				culture: 250
-			},
-			buildTime: 30000,
-			generates: {
-				stone: 8,
-				metal: 8,
-				wood: 8,
-				food: 16,
-				knowledge: 10,
-				culture: 10,
-				diplomacy: 5,
-				trade: 5,
-				peace: 5
-			},
-			citizenCap: 6
-			 */
-		} else if (sid.endsWith("storehouse")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				wood: 50,
-				stone: 50,
-				metal: 50,
-				food: 100
-			},
-			requirements: {
-				knowledge: 4,
-				culture: 4
-			},
-			buildTime: 120000,
-			generates: {
-				stone: 6,
-				metal: 6,
-				wood: 6,
-				food: 8,
-				economy: 3
-			},
-			citizenCap: 8
-			 */
-		} else if (sid.endsWith("barracks")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				wood: 100,
-				food: 200
-			},
-			requirements: {
-				knowledge: 40,
-				culture: 40
-			},
-			buildTime: 120000,
-			generates: {
-				wood: 10,
-				metal: 10,
-				stone: 10,
-				safety: 5,
-				peace: 5
-			},
-			citizenCap: 10
-			 */
-		} else if (sid.endsWith("blacksmith")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				wood: 50,
-				food: 100,
-				metal: 25,
-				stone: 25
-			},
-			requirements: {
-
-			},
-			buildTime: 240000,
-			generates: {
-				knowledge: 5,
-				stone: 5,
-				safety: 1,
-				diplomacy: 1
-			},
-			citizenCap: 8
-			 */
-		} else if (sid.endsWith("tower")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				stone: 25,
-				metal: 25,
-				food: 50
-			},
-			requirements: {
-				knowledge: 25,
-				culture: 25
-			},
-			buildTime: 120000,
-			generates: {
-				stone: 15,
-				knowledge: 5,
-				safety: 2
-			},
-			citizenCap: 10
-			 */
-		} else if (sid.endsWith("civic")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				stone: 1,
-				metal: 1,
-				food: 2
-			},
-			requirements: {
-				knowledge: 4,
-				culture: 4
-			},
-			buildTime: 30000,
-			generates: {
-				knowledge: 1,
-				culture: 1,
-				peace: 1
-			},
-			citizenCap: 20
-			 */
-		} else if (sid.endsWith("fortress")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				stone: 500,
-				metal: 200,
-				food: 1000
-			},
-			requirements: {
-				knowledge: 500,
-				culture: 500
-			},
-			buildTime: 300000,
-			generates: {
-				culture: 35,
-				knowledge: 35,
-				safety: 5,
-				peace: 5,
-				diplomacy: 5
-			},
-			citizenCap: 25
-			 */
-		} else if (sid.endsWith("temple")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			cost: {
-				stone: 500,
-				food: 5000
-			},
-			requirements: {
-				knowledge: 250,
-				culture: 250
-			},
-			buildTime: 600000,
-			generates: {
-				culture: 35,
-				spirituality: 15
-			},
-			citizenCap: 30 
-			 */
-		} else if (sid.endsWith("flag")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-			/*
-			 cost: {
-				stone: 0,
-				food: 0
-			},
-			requirements: {
-				knowledge: 0,
-				culture: 0
-			},
-			buildTime: 100000
-			 */
-		} else if (sid.endsWith("tree")) {
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-		} else {
-			System.out.println("ERROR - UNKNOWN STRUCTURE: " + sid);
-			return new StructureProperties(
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					new Resources(0, 0, 0, 0, 0, 0, 0, 0), 
-					0, 
-					0);
-		}
+		return null;	
 	}
 
 	public Resources getCost() {
@@ -386,5 +123,14 @@ public class StructureProperties {
 	public int getCitizenCap() {
 		return citizenCap;
 	}
+
+    /** Tries to parse a float value corresponding to propertyName in given propertyObject
+     * @param propertyObject JsonObject 
+     * @param propertyName
+     * @return Float value if property is found, 0 if no property can be found */
+    private static float parseNullableFloat(JsonObject propertyObject, String propertyName) {
+    	
+    	return propertyObject.get(propertyName) == null ? 0f : propertyObject.get(propertyName).asFloat();
+    }
 	
 }
